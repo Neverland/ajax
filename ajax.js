8e07d7c35d3884906e2c15f49f498031cb48ff00
Neverland <enix@foxmail.com>
2014-06-05 15:54:15 +0800

    Initial commit

---
+++ ajax.js
@@ -0,0 +1,49 @@
+/**
+ * @license
+ * Copyright 2012 enix
+ * enix@foxmail.com
+ * last Modified: 2014-06-01
+ */
+
+define('ajax', [], function () {
+
+	return function (method, url, data, callback, config) {
+		var xhr = new XMLHttpRequest,
+			t = [];
+
+		if (typeof(data) == 'function') {
+			callback = data;
+			data = null;
+			config = callback;
+		} else if (typeof(callback) != 'function') {
+			callback = function () {}
+		}
+
+		if (data != null && ({}).toString.call(data).indexOf('Object') > -1) {
+			for (var i in data) {
+				data.hasOwnProperty(i) && (t.push(i + '=' + data[i]));
+			}
+			data = t.join('&');
+		}
+
+		method = method.toUpperCase() || 'GET';
+
+		'GET' == method && data && data.length > 0 && (url += data);
+
+		xhr.addEventListener('readystatechange', function () {
+			xhr.readyState == 4 && /200|304/g.test(xhr.status) && callback(xhr.responseText, xhr.responseXML);
+		}, false)
+
+		xhr.addEventListener('timeout', function () {
+			typeof config.ontimeout == 'function' ? config.ontimeout() : alert('网络超时');
+		}, false)
+		xhr.timeout = isNaN(config.timeout) ? 4000 : config.timeout;
+
+		xhr.open(method || 'GET', url, true);
+		xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+		xhr.send(data || null);
+		return xhr;
+
+	}
+
+});
